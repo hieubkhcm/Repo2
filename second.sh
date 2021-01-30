84033efa5e6112e49508cbc535e3688d25e10746
hieubkhcm <hieubkhcm@gmail.com>
2021-01-30 17:16:02 +0800

    first commit note

--- second.sh
+++ second.sh
@@ -4,6 +4,6 @@ which ifconfig >/dev/null
 if [ "$?" != "0"  ]; then
 	echo "SyncAll error: The ifconfig utility is not install!" >&2
 	echo "Ubuntu and Linux Mint: RUn 'sudo apt install net-tools'" >&2
-	sudo apt install net-tool && -y
+	sudo apt install net-tool && -y 
 	ecit 1
 fi
